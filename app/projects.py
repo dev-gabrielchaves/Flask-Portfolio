918781e4bd10a6bb465aac740ff930437cda4e69
dev-gabrielchaves <dev.gchaves@gmail.com>
2023-12-13 16:56:47 -0300

    Added projects

---
+++ app/projects.py
@@ -0,0 +1,20 @@
+projects = [
+    {
+        'title' : 'Store Inventory Management',
+        'thumbnail' : 'imgs/store-inventory.png',
+        'categories' : ['Python', 'Flask', 'SQLite'],
+        'link': 'https://github.com/dev-gabrielchaves/Store-Inventory-Management'
+    },
+    {
+        'title' : 'Register Login System',
+        'thumbnail' : 'imgs/register-login.png',
+        'categories' : ['Python', 'Flask', 'SQLite'],
+        'link': 'https://github.com/dev-gabrielchaves/Simple-Register-Login-System-Flask'
+    },
+    {
+        'title' : 'CRUD of Users',
+        'thumbnail' : 'imgs/crud-flask.png',
+        'categories' : ['Python', 'Flask', 'SQLite'],
+        'link': 'https://github.com/dev-gabrielchaves/CRUD-Flask'
+    }
+]
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from app import app
 from flask import render_template
+from app.projects import projects
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', projects=projects)
 
 @app.route('/about')
 def about():
